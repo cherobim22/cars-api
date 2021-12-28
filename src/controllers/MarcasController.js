85592cc5177ac4a7deada3f9a944d71feaa2289c
Lucas cherobim <lucascherobim@hotmail.com>
2021-12-28 19:16:38 -0300

    refactor

--- src/controllers/MarcasController.js
+++ src/controllers/MarcasController.js
@@ -2,11 +2,11 @@ const connection = require('../database/connection');
 
 module.exports = {
     async create(req, res){
-        let errors=[]
-        const validate =  [ 'name', 'origin'] ;
+        let errors = [];
+        const validate =  ['name', 'origin'];
         const body = Object.keys(req.body);
         const difference = validate.filter(x => !body.includes(x));
-        const {name, origin } = req.body;
+        const {name, origin} = req.body;
         const date = new Date();
         const data = date.toLocaleString();
         const regex = new RegExp('/', 'g');
@@ -28,25 +28,27 @@ module.exports = {
     },
     async read(req, res){
         const page = req.query.page ?? 1;
-        let date = new Date()
+        const date = new Date();
         const now = date.toLocaleString(); 
         let string = '';
-        let start = "01/01/2021 00:00:00"
-        let end = now
+        let start = "01/01/2021 00:00:00";
+        let end = now;
 
         if(req.query.string){
-            string = req.query.string
+            string = req.query.string;
         }
 
         if(req.query.start && req.query.end){
-           start = req.query.start
-           end = req.query.end
+           start = req.query.start;
+           end = req.query.end;
         }
 
         const marcas = await connection('marcas').select('*')
-        .where((builder) => {
-            builder.whereBetween('created_at', [start, end]);
-        }).orderBy('id', 'desc').paginate({perPage:10,  currentPage: page});
+            .where((builder) => {
+                builder.whereBetween('created_at', [start, end]);
+            })
+            .orderBy('id', 'desc')
+            .paginate({perPage:10,  currentPage: page});
 
         return res.json(marcas);
     },
@@ -54,21 +56,21 @@ module.exports = {
     async readById(req, res){
         const { id } = req.params;
         const marca = await connection('marcas').where('id', '=', id).first();
-
+        
         return res.json(marca);
     },
     async update(req, res){
         const { id } = req.params;
-        let errors=[]
-        const validate =  [ 'nome', 'origin'] ;
+        let errors = [];
+        const validate =  ['nome', 'origin'];
         const body = Object.keys(req.body);
         let difference = validate.filter(x => !body.includes(x));
-        let date = new Date()
+        let date = new Date();
         let data = date.toLocaleString();
         let regex = new RegExp('/', 'g');
         let updated_at = data.replace(regex, '-');
         
-        let {nome, origin } = req.body;     
+        const {nome, origin} = req.body;     
 
         difference.forEach(e => {
             errors.push("Especifique o campo "+e);
@@ -86,10 +88,9 @@ module.exports = {
     async delete(req, res){
         const { id } = req.params;
         const product = await connection('marcas').where('id', id);
-        
-        let date = new Date()
+        const date = new Date();
         const deleted_at = date.toLocaleString();
-        let status = 0
+        const status = 0;
         
         if(product.length){
             await connection('marcas').where('id', id).update({status, deleted_at});
