7755dbb440c1c5b4006a228b9658f470d080a32d
cherobim22 <cherobim22@gmail.com>
2021-12-28 11:02:47 -0300

    update files

--- src/test/marcas.test.js
+++ src/test/marcas.test.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+
 const generate = () => {
     return crypto.randomBytes(20).toString('hex');
 }
@@ -6,7 +7,6 @@ const generate = () => {
 const request = function (url, method, data){
     return axios({url, method, data})
 }
-
 
 test('check if api response return array data greater than 0', async function () {
     const {data} = await request('http://localhost:3333/api/marcas')
