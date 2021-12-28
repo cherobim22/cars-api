bf3c0ee651167cf6cdb53d9c7653c2fdf2026ea2
lu057066 <cherobim22@gmail.com>
2021-12-28 11:01:43 -0300

    update files

--- src/test/marcas.test.js
+++ src/test/marcas.test.js
@@ -9,9 +9,8 @@ const request = function (url, method, data){
 
 
 test('check if api response return array data greater than 0', async function () {
-    const {data, status} = await request('http://localhost:3333/api/marcas')
-    console.log(data, status)
-    expect(data.length).toBeGreaterThan(120);
+    const {data} = await request('http://localhost:3333/api/marcas')
+    expect(data.length).toBeGreaterThan(0);
 });
 
 
