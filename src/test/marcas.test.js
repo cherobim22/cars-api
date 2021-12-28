056883876ba1ccffaf0d41bea86abd2d6f2c101e
Lucas cherobim <lucascherobim@hotmail.com>
2021-12-28 18:55:05 -0300

    update test

--- src/test/marcas.test.js
+++ src/test/marcas.test.js
@@ -1,16 +1,24 @@
 const axios = require('axios');
+const crypto = require('crypto');
 
 const generate = () => {
     return crypto.randomBytes(20).toString('hex');
 }
 
 const request = function (url, method, data){
-    return axios({url, method, data})
+    return axios({url, method, data});
 }
 
 test('check if api response return array data greater than 0', async function () {
     const {data} = await request('http://localhost:3333/api/marcas')
     expect(data.length).toBeGreaterThan(0);
+});
+
+test('check if api creates a new brand', async function () {
+    const body = {name:generate(), origin:generate()};
+    const {data} = await request('http://localhost:3333/api/marcas', 'POST', body);
+    const brand = await request('http://localhost:3333/api/marcas/'+data.resp[0]);
+    expect(brand.data.id).toBe(data.resp[0])
 });
 
 
