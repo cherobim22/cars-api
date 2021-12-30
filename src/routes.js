02f55b7a4e1f3b96b026301bb42d57fcde2101c3
Lucas cherobim <lucascherobim@hotmail.com>
2021-12-29 22:58:56 -0300

    update routes

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const routes = express.Router();
 
-const MarcasController = require('./controllers/MarcasController');
+const BrandasController = require('./controllers/BrandasController');
 
 // Root endpoint
 routes.get("/", (req, res) => {
     res.send('v1.0.0')
 });
-routes.get("/api/marcas/", MarcasController.read);
-routes.get("/api/marcas/:id", MarcasController.readById);
-routes.post("/api/marcas/", MarcasController.create);
-routes.delete("/api/marcas/:id", MarcasController.delete);
-routes.put("/api/marcas/:id", MarcasController.update);
+routes.get("/api/marcas/", BrandasController.read);
+routes.get("/api/marcas/:id", BrandasController.readById);
+routes.post("/api/marcas/", BrandasController.create);
+routes.delete("/api/marcas/:id", BrandasController.delete);
+routes.put("/api/marcas/:id", BrandasController.update);
 
 module.exports = routes;
