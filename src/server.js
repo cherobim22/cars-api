a7041501b53be7c2f82224f8543deda468811a9d
Lucas cherobim <lucascherobim@hotmail.com>
2022-01-06 22:49:25 -0300

    update server

--- src/server.js
+++ src/server.js
@@ -2,13 +2,6 @@
 const express = require("express");
 const routes = require('./routes');
 const app = express()
-const cors = require('cors');
-
-// var corsOptions = {
-//     origin: 'http://localhost:3000',
-//     optionsSuccessStatus: 200
-//   }
-// app.use(cors(corsOptions));
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
