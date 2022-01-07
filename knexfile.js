87e6c53f04e537be0609c5ac79f6a1e1b901a7d0
Lucas cherobim <lucascherobim@hotmail.com>
2022-01-06 22:49:44 -0300

    update db connection

--- knexfile.js
+++ knexfile.js
@@ -5,36 +5,24 @@ attachPaginate();
 
 module.exports = {
 
-  // development: {
-  //   client: 'sqlite3',
-  //   connection: {
-  //     filename: './src/database/db.sqlite3'
-  //   },
-  //   migrations: {
-  //     directory: './src/database/migrations'
-  //   },
-  //   useNullAsDefault: true,
-  // },
-  development:{
-    client: 'mysql',
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/database/db.sqlite3'
+    },
+    migrations: {
+      directory: './src/database/migrations'
+    },
+    useNullAsDefault: true,
+  },
+  production: {
+    client: 'postgresql',
     connection: {
       host : process.env.HOST,
       port : process.env.PORT,
       user : process.env.USER,
       password : process.env.PASSWORD,
       database : process.env.DATABASE
-    },
-    migrations: {
-      directory: 'src/database/migrations'
-    },
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
     },
     pool: {
       min: 2,
