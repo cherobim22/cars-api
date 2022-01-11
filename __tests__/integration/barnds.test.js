75b6aa52cd4add6efe1325ba3cea56a8803cc8d3
Lucas cherobim <lucascherobim@hotmail.com>
2022-01-10 22:40:33 -0300

    api tests

--- __tests__/integration/barnds.test.js
+++ __tests__/integration/barnds.test.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const randomstring = require("randomstring");
 
 const request = function (url, method, data){
     return axios({url, method, data});
@@ -10,4 +11,26 @@ describe('getting information', () => {
         let itens = res.data;
         expect(itens.data.length).toBeGreaterThan(1);
     }); 
+
+    it('should recieve brands and match object', async () => {
+        const res = await request('http://localhost:3333/api/marcas')
+        let itens = res.data;
+        expect(itens.data[0]).toHaveProperty('name');
+    }); 
+
+    it('must create and delete a brand', async () => {
+        let mock = {
+            "name": randomstring.generate(8),
+            "origin":  randomstring.generate(8)
+        };
+        const res = await request('http://localhost:3333/api/marcas', 'POST', mock)
+        let item = res.data.id;
+        expect(res.status).toEqual(200)
+        expect(res.data).toHaveProperty('id');
+
+        const {data} = await request('http://localhost:3333/api/marcas/'+item, 'DELETE')
+
+        expect(data.success).toEqual(1)
+    }); 
+
 });
