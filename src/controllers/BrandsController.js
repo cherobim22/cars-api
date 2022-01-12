fc612ebce3d54ec02120048d5b4ef84d255e6262
Lucas cherobim <lucascherobim@hotmail.com>
2022-01-11 21:45:34 -0300

    update controllers

--- src/controllers/BrandsController.js
+++ src/controllers/BrandsController.js
@@ -1,96 +1,103 @@
-const brandService = require('../services/brandService');
+const brandService = require("../services/brandService");
+const router = require("express").Router();
 
-module.exports = {
-    async create(req, res){
-        let errors = [];
-        const validate =  ['name', 'origin'];
-        const body = Object.keys(req.body);
-        const difference = validate.filter(x => !body.includes(x));
-        const {name, origin} = req.body;
-        const date = new Date();
-        const data = date.toLocaleString();
-        const regex = new RegExp('/', 'g');
-        const created_at = data.replace(regex, '-');
+router.post("/", (req, res) => {
+  let errors = [];
+  const validate = ["name", "origin"];
+  const body = Object.keys(req.body);
+  const difference = validate.filter((x) => !body.includes(x));
+  const { name, origin } = req.body;
+  const date = new Date();
+  const data = date.toLocaleString();
+  const regex = new RegExp("/", "g");
+  const created_at = data.replace(regex, "-");
 
-        difference.forEach(e => {
-            errors.push("Especifique o campo "+e);
-        });
+  difference.forEach((e) => {
+    errors.push("Especifique o campo " + e);
+  });
 
-        if (errors.length){
-            res.status(400).json({"error":errors});
-            return;
-        }
-        const marca = await brandService.getBy('name', name);
+  if (errors.length) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+  const marca = await brandService.getBy("name", name);
 
-        if(marca){
-            res.status(422).json({"error":"this brand already exists"});
-        }
+  if (marca) {
+    res.status(422).json({ error: "this brand already exists" });
+  }
 
-        let resp =  await brandService.insertBrand(name, origin, created_at)
-        return res.json({id: resp});
-    },
-    async read(req, res){
-        const page = req.query.page ?? 1;
-        const date = new Date();
-        const now = date.toLocaleString(); 
-        let string = '';
-        let start = "01/01/2021 00:00:00";
-        let end = now;
+  let resp = await brandService.insertBrand(name, origin, created_at);
+  return res.json({ id: resp });
+});
 
-        if(req.query.string){
-            string = req.query.string;
-        }
+router.get("/", (req, res) => {
+  const page = req.query.page ?? 1;
+  const date = new Date();
+  const now = date.toLocaleString();
+  let string = "";
+  let start = "01/01/2021 00:00:00";
+  let end = now;
 
-        if(req.query.start && req.query.end){
-           start = req.query.start;
-           end = req.query.end;
-        }
+  if (req.query.string) {
+    string = req.query.string;
+  }
 
-        const brands = await brandService.getBrands(start, end, page);
+  if (req.query.start && req.query.end) {
+    start = req.query.start;
+    end = req.query.end;
+  }
 
-        return res.json(brands);
-    },
+  const brands = await brandService.getBrands(start, end, page);
 
-    async readById(req, res){
-        const { id } = req.params;
-        const marca = await brandService.getBy('id', id);
-        
-        return res.json(marca);
-    },
-    async update(req, res){
-        const { id } = req.params;
-        let errors = [];
-        const validate =  ['nome', 'origin'];
-        const body = Object.keys(req.body);
-        let difference = validate.filter(x => !body.includes(x));
-        let date = new Date();
-        let data = date.toLocaleString();
-        let regex = new RegExp('/', 'g');
-        let updated_at = data.replace(regex, '-');
-        
-        const {name, origin} = req.body;     
+  return res.json(brands);
+});
 
-        difference.forEach(e => {
-            errors.push("Especifique o campo "+e);
-        });
-    
-        if (errors.length){
-            res.status(400).json({"error":errors});
-            return;
-        }
-        
-        await brandService.updateBrand(id, name, origin, updated_at)
-        return res.json({ msg: 'atualizado com sucesso'});
-    },
-    async delete(req, res){
-        const { id } = req.params;
-        const brand = await brandService.getBy('id', id);
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const marca = await brandService.getBy("id", id);
 
-        if(brand.length){
-            const resp = await brandService.deleteBrand(id);
-            return res.status(200).json({msg: 'removido com sucesso', success: resp});
-        }else{
-           return res.status(404).json({msg: 'nenhuma marca encontrada com o id '+id});
-        }
-    }
-};
+  return res.json(marca);
+});
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  let errors = [];
+  const validate = ["nome", "origin"];
+  const body = Object.keys(req.body);
+  let difference = validate.filter((x) => !body.includes(x));
+  let date = new Date();
+  let data = date.toLocaleString();
+  let regex = new RegExp("/", "g");
+  let updated_at = data.replace(regex, "-");
+
+  const { name, origin } = req.body;
+
+  difference.forEach((e) => {
+    errors.push("Especifique o campo " + e);
+  });
+
+  if (errors.length) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  await brandService.updateBrand(id, name, origin, updated_at);
+  return res.json({ msg: "atualizado com sucesso" });
+});
+
+router.delete("/", (req, res) => {
+  const { id } = req.params;
+  const brand = await brandService.getBy("id", id);
+
+  if (brand.length) {
+    const resp = await brandService.deleteBrand(id);
+    return res.status(200).json({ msg: "removido com sucesso", success: resp });
+  } else {
+    return res
+      .status(404)
+      .json({ msg: "nenhuma marca encontrada com o id " + id });
+  }
+});
+
+
+module.exports = app => app.use('/api/marcas/', router);
